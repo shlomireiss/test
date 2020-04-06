a9f2e79867e9b22fbc5454eb4638994ec212e8af
shlomireiss <shlomi.reiss@yahoo.com>
2020-04-06 13:17:12 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
-import "styles.css";
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
 
-document.getElementById("app").innerHTML = `
-<h1>Title</h1>
-<div>
-HEllo
-</div>
-`;
+ctx.fillStyle = 'rgb(0,0,0)';
+ctx.fillRect(0,0,10,10);
+
+ctx.fillStyle = 'rgb(255,0,0)';
+ctx.fillRect(50,50,100,150);
+
+ctx.fillStyle = 'rgb(0,255,0)';
+ctx.fillRect(75,75,100,100);
+
+ctx.fillStyle = 'rgba(255,0,255,0.75)';
+ctx.fillRect(25,100,175,50);
+
+ctx.strokeStyle = 'rgb(255,255,255)';
+ctx.lineWidth = 5;
+ctx.strokeRect(25,25,175,200);
